bf2fe3424b1966a83101da55b1a62224b261518d
workerwalrus <58191626+workerwalrus@users.noreply.github.com>
2026-04-21 11:08:59 -0700

    Update SecLibGateway.php

--- src/SecLibGateway.php
+++ src/SecLibGateway.php
@@ -382,7 +382,7 @@ class SecLibGateway implements GatewayInterface
      */
     public function nextLine()
     {
-        $value = $this->getConnection()->_get_channel_packet(\phpseclib3\Net\SSH2::CHANNEL_EXEC);
+        $value = $this->getConnection()->get_channel_packet(\phpseclib3\Net\SSH2::CHANNEL_EXEC);
 
         return $value === true ? null : $value;
     }
